f3b5cb382182e7ce143ed6cdddf4bffee27ee56e
Sejin Kim <froydroyce@gmail.com>
2019-10-09 17:22:47 -0600

    Create DELETE food from meal request test
    Co-Authored-By: S. Mainar <smainar@users.noreply.github.com>

---
+++ tests/delete_food_from_meal_request.spec.js
@@ -0,0 +1,42 @@
+const shell = require('shelljs');
+const request = require('supertest');
+const app = require('../app');
+const food = require('../models').Food;
+const meal = require('../models').Meal;
+const mealFood = require('../models').MealFood;
+
+describe('Meals API', () => {
+  describe('DELETE food from meal request', () => {
+    beforeEach(async () => {
+      await mealFood.destroy({where: {}});
+      await meal.destroy({where: {}});
+      await food.destroy({where: {}});
+    });
+    afterEach(async () => {
+      await mealFood.destroy({where: {}});
+      await meal.destroy({where: {}});
+      await food.destroy({where: {}});
+    });
+
+    test('It returns a status code of 204', async () => {
+      const apple = await food.create({
+        "name": "Apple",
+        "calories": 220
+      });
+
+      const parfait = await meal.create({
+        name: 'Yoplait Parfait'
+      })
+
+      await mealFood.create({
+        foodId: apple.id,
+        mealId: parfait.id
+      })
+
+      return request(app).delete(`/api/v1/meals/${parfait.id}/foods/${apple.id}`)
+        .then(response => {
+          expect(response.status).toBe(204)
+        })
+    })
+  })
+})
