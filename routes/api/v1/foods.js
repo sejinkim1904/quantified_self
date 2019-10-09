7c06e11535bfe1d80465939f74ca8fe794a05ae7
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-08 18:36:56 -0600

    Add route and controller action for GET '/api/v1/foods/:id'
    Co-Authored-By: Sejin Kim <froydroyce@users.noreply.github.com>

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -15,4 +15,28 @@ router.get('/', async (req, res, next) => {
     })
 })
 
+/* GET single food */
+router.get('/:id', async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  await food.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(foundFood => {
+      if (!foundFood) {
+        payload = {
+          error: 'Food does not exist.',
+          status: 404
+        }
+        res.status(404).send(payload)
+        return;
+      }
+        res.status(200).send(foundFood)
+    })
+    .catch(async error => {
+      res.status(500).send({ error })
+    })
+})
+
 module.exports = router;
