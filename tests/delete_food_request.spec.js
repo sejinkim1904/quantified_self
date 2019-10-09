d8fbc456782f6fc1e51e7855fea3f222d61368ba
Sejin Kim <froydroyce@gmail.com>
2019-10-09 14:02:13 -0600

    Create DELETE food request test
    Co-Authored-By: S. Mainar <smainar@users.noreply.github.com>

---
+++ tests/delete_food_request.spec.js
@@ -0,0 +1,27 @@
+const shell = require('shelljs');
+const request = require('supertest');
+const app = require('../app');
+const food = require('../models').Food;
+
+describe('api', () => {
+  describe('Delete Food request', () => {
+    beforeEach(async () => {
+      await food.destroy({where: {}});
+    });
+    afterEach(async () => {
+      await food.destroy({where: {}});
+    });
+
+    test('It returns status code of 204', async () => {
+      const avocado = await food.create({
+        name: 'avocado',
+        calories: 9000
+      });
+
+      return request(app).delete(`/api/v1/foods/${avocado.id}`)
+        .then(response => {
+          expect(response.status).toBe(204)
+        })
+    })
+  })
+})
