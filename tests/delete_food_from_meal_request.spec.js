b47d058f603386233f7d52b73ec57f3d992daeef
Stella Mainar <mainars09@alumni.hanover.edu>
2019-10-10 14:34:39 -0600

    Add sad path test for DELETE food from meal
    Co-Authored-By: Sejin Kim <froydroyce@users.noreply.github.com>

--- tests/delete_food_from_meal_request.spec.js
+++ tests/delete_food_from_meal_request.spec.js
@@ -38,5 +38,30 @@ describe('Meals API', () => {
           expect(response.status).toBe(204)
         });
     });
+
+    test('It returns a 404 if food is not found', async () => {
+      const parfait = await meal.create({
+        name: 'Yoplait Parfait'
+      });
+
+      return request(app).delete(`/api/v1/meals/${parfait.id}/foods/90`)
+        .then(response => {
+          expect(response.status).toBe(404)
+          expect(response.body.message).toBe('Food not found.')
+        });
+    });
+
+    test('It returns a 404 if meal is not found', async () => {
+      const apple = await food.create({
+        "name": "Apple",
+        "calories": 220
+      });
+
+      return request(app).delete(`/api/v1/meals/90/foods/${apple.id}`)
+        .then(response => {
+          expect(response.status).toBe(404)
+          expect(response.body.message).toBe('Meal not found.')
+        });
+    });
   });
 });
